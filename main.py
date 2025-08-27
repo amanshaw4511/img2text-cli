db2fe9db4d2d1fe050f7be0000603b57afeb7215
Aman Shaw <amanshaw4511@protonmail.com>
2025-08-28 01:04:08 +0530

    Initialize project

---
+++ main.py
@@ -0,0 +1,58 @@
+import io
+from PIL import Image
+import pytesseract
+import sys
+import click
+import pyperclipimg
+import pyperclip
+
+
+def get_img_data(filename: str | None):
+    if sys.stdin.isatty():
+        if filename is None:
+            raise click.UsageError("No file provided")
+        return open(filename, "rb")
+
+    data = sys.stdin.buffer.read()
+    return io.BytesIO(data)
+
+
+@click.command(
+    name="ocr",
+    epilog="""
+Examples:\n
+- Image file to text\n
+   $ img2text image.png\n
+- Piped image to text\n
+   $ cat image.png | img2text\n
+- Extract text from the clipboard image and copy it to the clipboard\n
+   $ img2text --clip\n
+""",
+)
+@click.argument("image file", type=click.Path(exists=True), required=False)
+@click.option(
+    "--clip",
+    is_flag=True,
+    help="Extract text from the clipboard image and copy it to the clipboard",
+)
+def ocr(clip: bool, filename: str | None):
+    """
+    Extract text from an image
+    """
+    if clip:
+        clip_ocr()
+        return
+
+    image = Image.open(get_img_data(filename))
+    text = pytesseract.image_to_string(image=image)
+    print(text)
+
+
+def clip_ocr():
+    image = pyperclipimg.paste()
+    text = pytesseract.image_to_string(image=image)
+    pyperclip.copy(text)
+
+
+if __name__ == "__main__":
+    ocr()
